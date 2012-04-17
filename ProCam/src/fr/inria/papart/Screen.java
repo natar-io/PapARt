468df3d6803a36f2fa778529f2d27d380526fa71
Jeremy LAVIOLE <jeremylaviole@MacBook-Pro-de-Jeremy-LAVIOLE.local>
2012-04-17 17:32:17 +0200

    adding new project procam

---
+++ ProCam/src/fr/inria/papart/Screen.java
@@ -0,0 +1,14 @@
+package fr.inria.papart;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jeremylaviole
+ */
+public class Screen {
+    
+}
