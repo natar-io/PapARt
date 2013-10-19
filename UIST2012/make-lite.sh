4a619b17086a90a964f2cb7230a0e2c541593f0e
jeremy Laviole <jiii@crunchbang.(none)>
2013-10-19 16:03:08 +0200

    nettoyage du dépôt
    Former-commit-id: 2968638e539859a34390ce30f47385fed23ae383

--- UIST2012/make-lite.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash 
-
-gs -q -dSAFER -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dPDFSETTINGS=/ebook -sOUTPUTFILE=poster-lite.pdf -f poster.pdf
-
-gs -q -dSAFER -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dPDFSETTINGS=/ebook -sOUTPUTFILE=demo-lite.pdf -f demo.pdf
-
-gs -q -dSAFER -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dPDFSETTINGS=/ebook -sOUTPUTFILE=doct-lite.pdf -f doct.pdf
