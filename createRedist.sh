164ed7a8afd9f840650002aa77f087d007350e98
Jeremy Laviole <jiii@jiii.fr>
2013-11-27 14:22:50 +0100

    Folder scripts for examples
    Former-commit-id: aea5ecd9a44f34166c4b4d83a28b23e6336b9b7a

--- createRedist.sh
+++ createRedist.sh
@@ -10,6 +10,9 @@ cp ProCam/dist/ProCam.jar ProCamLibrary/library/ProCamLibrary.jar
 cp -R ProCam/src ProCamLibrary/src/
 
 cp -R $SKETCHBOOK/examples/* ProCamLibrary/examples/
+cp -R $SKETCHBOOK/common ProCamLibrary/common
+cp -R $SKETCHBOOK/calib ProCamLibrary/examples/calib
+#cp -R $SKETCHBOOK/data ProCamLibrary/data
 
 tar -zcf ProCamLibrary.tgz ProCamLibrary
 
