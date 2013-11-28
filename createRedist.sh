82b56187c6b5648ba8cc4f5882f1fee8ca4b5d3f
Jeremy Laviole <jiii@jiii.fr>
2013-11-28 15:39:26 +0100

    Added Windows dependencies - updates on the library creation script
    Former-commit-id: 3e862ec3fc3b562958ae7d560a938a5e3560fc2e

--- createRedist.sh
+++ createRedist.sh
@@ -6,16 +6,40 @@ mkdir ProCamLibrary
 mkdir ProCamLibrary/library
 mkdir ProCamLibrary/examples
 
-cp ProCam/dist/ProCam.jar ProCamLibrary/library/ProCamLibrary.jar
-cp -R ProCam/src ProCamLibrary/src/
+echo "Copy Library"
 
+# Library
+cp ProCam/dist/ProCam.jar ProCamLibrary/library/ProCamLibrary.jar
+
+echo "Copy JavaCV, OpenCV and friends"
+
+# libs are  javaCV and javaCV cppjars
+cp libs/* ProCamLibrary/library/
+
+
+echo "Copy the sources" 
+# copy the source also
+cp -R ProCam/src ProCamLibrary/
+
+echo "Copy Examples - Common and Calib" 
+# Examples
 cp -R $SKETCHBOOK/examples/* ProCamLibrary/examples/
+
+# Source to create a new sketch
 cp -R $SKETCHBOOK/common ProCamLibrary/common
-cp -R $SKETCHBOOK/calib ProCamLibrary/examples/calib
+cp -R $SKETCHBOOK/calib/config/config.pde ProCamLibrary/common/
+
+# Calibration stuff
+cp -R $SKETCHBOOK/calib ProCamLibrary/calib
+
 #cp -R $SKETCHBOOK/data ProCamLibrary/data
 
+
+echo "Create the archive..." 
 tar -zcf ProCamLibrary.tgz ProCamLibrary
 
+
+echo "Clean " 
 rm -rf ProCamLibrary
 
 
