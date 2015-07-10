95dccac7fe86564da283dd63667b5ab6dcab315b
Jeremy Laviole <jeremy.laviole@inria.fr>
2015-07-10 14:07:22 +0200

    presentation update.
    Former-commit-id: db975f543d12089fb7d2421ad9f02bac1dfd926a

--- communication/robot-teaching/start.rb
+++ communication/robot-teaching/start.rb
@@ -2,60 +2,12 @@ require 'soby'
 
 # Give the current folder to Processing.
 Processing::App::SKETCH_PATH = __FILE__
+$:.unshift File.dirname(__FILE__)
 
-$app = nil
-$app =  SobyPlayer.new 1920,1080 if $app == nil
+$app =  SobyPlayer.new 1920,1080
 
 sleep 0.2 while not $app.ready? 
 
-$:.unshift File.dirname(__FILE__)
+presentation = Soby::load_presentation  'papart.rb', 'robot.svg'
+Soby::auto_update presentation, __FILE__
 
-@main_file = 'robot.svg'
-
-def load_prez
-  load 'papart.rb'
-  @robot_prez = Presentation.new($app, $app.sketchPath(@main_file))
-  $app.robot_prez = @robot_prez
-end
-
-def start_prez
-  $app.set_prez @robot_prez  
-end
-
-def find_files
-  @files = Dir.glob(File.join(SKETCH_ROOT, "**/*.{svg,glsl,rb}"))
-
-  f = nil
-  @files.each do  |filename|
-    f = filename  if filename.end_with? "start.rb"
-  end
-  
-  @files.delete(f)
-end
-
-
-if $app != nil 
-  find_files
-  load_prez
-  start_prez
-  
-
-  @time = Time.now 
-  SLEEP_TIME = 1
-  
-  t = Thread.new {
-    loop do
-      if @files.find { |file| FileTest.exist?(file) && File.stat(file).mtime > @time }
-        puts 'reloading sketch...'
-        
-        @time = Time.now
-        
-        load 'papart.rb'
-        load_prez
-        start_prez
-        
-      end
-      sleep SLEEP_TIME
-    end
-  }
-end
