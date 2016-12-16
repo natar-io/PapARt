1430176c129f81fc812e71fcbe18f3fdb4151e3b
Jeremy Laviole <poqudrof@gmail.com>
2016-12-16 17:44:49 +0100

    Refactor tests.

--- papart/test/fr/inria/papart/UtilsTest.java
+++ papart/test/fr/inria/papart/UtilsTest.java
@@ -19,7 +19,7 @@
  */
 package fr.inria.papart;
 
-import fr.inria.papart.procam.Utils;
+import fr.inria.papart.procam.utils.ARToolkitPlusUtils;
 import org.bytedeco.javacpp.opencv_core.IplImage;
 import processing.core.PApplet;
 
