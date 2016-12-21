235c2e920d3e5bea7cbd3636a33919c0bf37cb02
Jeremy Laviole <poqudrof@gmail.com>
2016-12-21 12:17:49 +0100

    save current branch.

--- createAll.rb
+++ createAll.rb
@@ -2,9 +2,10 @@
 # coding: utf-8
 
 
-`sh downloadExamples.sh`
-`sh createLibs.sh`
+# %x(sh downloadExamples.sh)
+# %x(sh createLibs.sh)
 
+current_branch = %x(git rev-parse --abbrev-ref HEAD).chomp
 
 output_names=["realsense"]
               # "kinect",
@@ -12,9 +13,12 @@ output_names=["realsense"]
               # "hardware3"]
 
 output_names.each do |release_name|
+  puts "Get the version: " + release_name
   %x(git checkout release-#{release_name})
+  puts "Build it."
   %x(sh build.sh)  
+  puts "Create redistribuable."
   %x(sh createRedist.sh #{release_name}) 
 end
 
-
+%x(git checkout #{current_branch})
