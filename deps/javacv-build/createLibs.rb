6d9fff2126df8c1b50ae13d1f9dc4fb883420464
jeremy laviole <jeremy.laviole@inria.fr>
2017-01-04 12:44:50 +0100

    first version of javacv library deployment for OSX.

--- deps/javacv-build/createLibs.rb
+++ deps/javacv-build/createLibs.rb
@@ -1,7 +1,7 @@
 #!/bin/ruby
 
 
-platforms = ["linux", "windows"]
+platforms = ["linux", "windows", "macosx"]
 archs = ["x86_64", "x86"]
 
 
@@ -9,28 +9,30 @@ def build(platform, arch)
 
   puts "Build " + platform
   %x(cp pom-#{platform}.xml pom.xml)
-  
+
   puts "Get the library"
 
   %x(mvn -Dplatform=#{platform}-#{arch} dependency:copy-dependencies)
 
   %x(rm target/dependency/*linux*)   if platform.eql? "windows"
+  %x(rm target/dependency/*linux*)   if platform.eql? "macosx"
 
   `mv target/dependency target/library`
   `mv target javacv`
   `mv javacv/library/javacv-1.3.jar javacv/library/javacv.jar`
-  
+
   puts "compress library"
   `tar -zcf javacv-#{platform}-#{arch}.tgz javacv`
   `rm -rf javacv`
-  
+
   puts "done javacv-" + platform + "-" + arch
-  
+
   %x(rm pom.xml)
 end
 
 build("linux", "x86_64")
 build("windows", "x86_64")
+build("macosx", "x86_64")
 build("windows", "x86")
 
 # mvn -Dplatform=windows-x86_64 dependency:copy-dependencies
