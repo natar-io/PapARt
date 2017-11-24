a02f3b6843e6fd9acf327e9ee82753f6f9f829ed
Jeremy Laviole <poqudrof@gmail.com>
2017-11-24 19:29:48 +0100

    AppRunner first implementation.

---
+++ papart/src/fr/inria/papart/compositor/XAppRunner.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.inria.papart.compositor;
+
+import java.io.IOException;
+import java.lang.ProcessBuilder.Redirect;
+import static java.lang.Thread.sleep;
+import java.lang.reflect.Field;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author jiii
+ */
+public class XAppRunner extends AppRunner {
+
+    private XDisplay targetDisplay;
+
+    public XAppRunner(String program, XDisplay target) {
+        super(program);
+        this.targetDisplay = target;
+    }
+    public XAppRunner(String[] program, XDisplay target) {
+        super(program);
+        this.targetDisplay = target;
+    }
+
+    @Override
+    protected void buildProcess() {
+        super.buildProcess();
+
+        if (this.targetDisplay != XDisplay.NO_DISPLAY) {
+            env.put("DISPLAY", targetDisplay.name());
+        }
+    }
+}
---
+++ papart/src/fr/inria/papart/compositor/XDisplay.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.inria.papart.compositor;
+
+import java.io.IOException;
+import java.lang.ProcessBuilder.Redirect;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author jiii
+ */
+public class XDisplay extends AppRunner {
+
+    public static final XDisplay NO_DISPLAY = new XDisplay(0, 0);
+    public static int nextDisplayID = 99;
+
+    private final int id;
+    private final int width, height;
+    private final int depth = 16;
+    private final String xvfbName = "/usr/bin/Xvfb";
+
+    // For the NO_DISPLAY  constant.
+    private XDisplay() {
+        id = -1;
+        width = 0;
+        height = 0;
+    }
+
+    public XDisplay(int width, int height) {
+        super();
+        id = nextDisplayID--;
+        this.width = width;
+        this.height = height;
+        System.out.println("Preparing XDisplay: " + id + " .");
+    }
+
+    @Override
+    protected void buildProcess() {
+        String[] cmd = new String[]{
+            xvfbName, ":" + id,
+            "-ac", "-screen", "0 ", width + "x" + height + "x" + depth
+        };
+
+        builder = new ProcessBuilder(cmd);
+
+        // DEBUG
+        builder.redirectOutput(Redirect.INHERIT);
+        builder.redirectError(Redirect.INHERIT);
+
+        Map<String, String> env = builder.environment();
+        env.put("PATH", "/bin:/usr/bin");
+    }
+
+    @Override
+    public String toString() {
+        return ":" + id;
+    }
+
+    public String name() {
+        return ":" + id;
+    }
+
+    public int getDisplayId() {
+        return id;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+}
