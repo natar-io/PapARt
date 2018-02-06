fa267d28e82b51139ac55fc6df6cc236cba085f7
Jeremy Laviole <laviole@rea.lity.tech>
2018-02-06 13:00:28 +0100

    cleaning

--- papart/lib/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-
-mvn install:install-file -DlocalRepositoryPath=repo -DcreateChecksum=true -Dpackaging=jar -Dfile=org.openni.jar -DgroupId=org.openni -DartifactId=OpenNI -Dversion=2.3
-
-mvn install:install-file -DlocalRepositoryPath=repo -DcreateChecksum=true -Dpackaging=jar -Dfile=openni-bin.jar -DgroupId=org.openni -DartifactId=OpenNI-bin -Dversion=2.3
