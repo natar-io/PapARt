54e896b374ae1412a1b347c4d6cf058490a439ad
Jeremy Laviole <laviole@rea.lity.tech>
2018-03-08 16:33:09 +0100

    commit before new method test.

--- papart/src/fr/inria/papart/multitouch/detection/LineCluster.java
+++ papart/src/fr/inria/papart/multitouch/detection/LineCluster.java
@@ -9,6 +9,8 @@ import Jama.Matrix;
 import com.mkobos.pca_transform.PCA;
 import fr.inria.papart.multitouch.tracking.TrackedElement;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Objects;
 import java.util.function.Predicate;
 import processing.core.PVector;
 
@@ -16,6 +18,16 @@ import processing.core.PVector;
  *
  * @author realitytech
  */
+class LinePair{
+    public TrackedElement t1, t2;
+    
+    public boolean equals(Object o){
+        LinePair p = (LinePair ) o;
+        
+        return (t1 == p.t1 && t2 == p.t2) || 
+                (t1 == p.t2 && t2 == p.t1);
+    }
+}
 
 public class LineCluster extends StickerCluster {
 
@@ -25,6 +37,9 @@ public class LineCluster extends StickerCluster {
             TrackedElement t1,
             TrackedElement t2,
             TrackedElement t3) {
+        assert(t1 != t2);
+        assert(t1 != t3);
+        assert(t2 != t3);
         line.add(t1);
         line.add(t2);
         line.add(t3);
@@ -32,19 +47,18 @@ public class LineCluster extends StickerCluster {
 
     private double angle = Float.MIN_VALUE;
 
-    public PVector position(){
+    public PVector position() {
         PVector mean = new PVector();
-        for (TrackedElement t : line){
+        for (TrackedElement t : line) {
             mean.add(t.getPosition());
         }
-        mean.mult(1f/ line.size());
+        mean.mult(1f / line.size());
         return mean;
     }
-    public double angle() {
-        if (angle != Float.MIN_VALUE) {
-            return angle;
-        }
 
+    public boolean highAngle = false;
+    
+    private void computeAngle() {
         double[][] dataPoints = new double[line.size()][2];
         for (int i = 0; i < line.size(); i++) {
             dataPoints[i][0] = line.get(i).getPosition().x;
@@ -55,40 +69,78 @@ public class LineCluster extends StickerCluster {
         PCA pca = new PCA(trainingData);
         Matrix vectors = pca.getEigenvectorsMatrix();
 
-        try {
-            if (pca.getEigenvectorsMatrix().getColumnDimension() != 2
-                    || pca.getEigenvectorsMatrix().getRowDimension() != 2) {
-                System.out.println("EigenMatrix too short. ?");
-            } else {
+        if (pca.getEigenvectorsMatrix().getColumnDimension() != 2
+                || pca.getEigenvectorsMatrix().getRowDimension() != 2) {
 
-                // Size in X?
-                double e0 = pca.getEigenvalue(0);
-                // Size in Y // should be very low.
-                double e1 = pca.getEigenvalue(1);
+//                System.out.println("EigenMatrix too short. ?");
+//                for (int i = 0; i < line.size(); i++) {
+//                    System.out.println("id: " + i + " " + line.get(i).getPosition());
+//                }
+            // Short eigen matrix -> Points aligned according to an existing axis. 
+            float x = line.get(0).getPosition().x;
+            float y = line.get(0).getPosition().y;
+            int xCount = 1;
+            int yCount = 1;
 
-                // check that e1 is very low.
-                if (e1 > 2) {
-//                    System.out.println("e1 high: " + e1);
+            for (int i = 1; i < line.size(); i++) {
+                float x1 = line.get(i).getPosition().x;
+                float y1 = line.get(i).getPosition().y;
+
+                if (x == x1) {
+                    xCount++;
                 }
-//                eigenValue.set((float) e0, (float) e1);
-//                eigenVector.set((float) vectors.get(0, 0), (float) vectors.get(0, 1));
-
-                angle = Math.atan2(vectors.get(0, 0), vectors.get(0, 1));
-
+                if (y == y1) {
+                    yCount++;
+                }
             }
-        } catch (Exception e) {
-            System.out.println("Erreur in pca" + e);
-            e.printStackTrace();
+            // same X
+            if (xCount == line.size()) {
+                angle = 0;
+            }
+            // same Y
+            if (yCount == line.size()) {
+                angle = Math.PI;
+            }
+
+        } else {
+
+            // Size in X?
+            e0 = pca.getEigenvalue(0);
+            // Size in Y // should be very low.
+            e1 = pca.getEigenvalue(1);
+
+            // check that e1 is very low.
+//            if (e1 > 2) {
+////                System.out.println("e1 high: " + e1);
+//                highAngle = true;
+//            }
+//            System.out.println("e0: " + e0);
+            angle = Math.atan2(vectors.get(0, 1), vectors.get(0, 0));
+        }
+    }
+
+    public double e0, e1;
+
+    public double angle() {
+        if (angle != Float.MIN_VALUE) {
+            return angle;
+        }else{
+            computeAngle();
         }
         return angle;
     }
 
     @Override
     public boolean equals(Object other) {
-        return ((StickerCluster) other).containsAll(line);
+        return ((LineCluster) other).line.containsAll(line);
     }
-    
-    
+
+//    @Override
+//    public int hashCode() {
+//        int hash = 7;
+//        hash = 83 * hash + Objects.hashCode(this.line);
+//        return hash;
+//    }
     /**
      * TODO:
      *
@@ -113,12 +165,19 @@ public class LineCluster extends StickerCluster {
 //         take an element.
         ArrayList<LineCluster> lines = new ArrayList<>();
 
-        for (TrackedElement first : elements) {
+        Iterator<TrackedElement> it = elements.iterator();
 
+        // With iterators, we can delete elements on the fly. 
+        while (it.hasNext()) {
+            TrackedElement first = it.next();;
+
+//        for (TrackedElement first : elements) {
 //        TrackedElement first = elements.get(0);
             // Get the closets elements
             ArrayList<TrackedElement> elementsCopy = new ArrayList<TrackedElement>(elements);
             elementsCopy.remove(first);
+            
+            // Deep search from this.
 
             Predicate<TrackedElement> closeFilter = te -> te.distanceTo(first) > size;
             elementsCopy.removeIf(closeFilter);
@@ -146,17 +205,19 @@ public class LineCluster extends StickerCluster {
 
                         if (!lines.contains(lineCluster)) {
                             lines.add(lineCluster);
+                            lineCluster.computeAngle();
                         }
                     }
                 }
-            }
 
+            }
+            it.remove();
         }
 
         return lines;
     }
 
-    public static float epsilon = 10f;
+    public static float epsilon = 15f;
 
     /**
      * Check if 3 points are aligned.
@@ -170,8 +231,8 @@ public class LineCluster extends StickerCluster {
      */
     public static boolean pointsColinear(PVector a, PVector b, PVector c) {
         float area = (a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y)) / 2;
-        System.out.println("area: " + area);
-            return Math.abs(area) < epsilon;
+//        System.out.println("area: " + area);
+        return Math.abs(area) < epsilon;
     }
 
 }
