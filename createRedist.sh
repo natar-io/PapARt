91c2e4ca59f50c2be15fdc19914f5dea7a1a440e
Nicolas PALARD <palardnicolas@gmail.com>
2019-04-25 18:19:01 +0200

    :arrow_up: Small update on dependencies & scripts

--- createRedist.sh
+++ createRedist.sh
@@ -17,6 +17,7 @@ echo "Copy Library"
 cp papart/target/$NAME.jar $TMP/$NAME/library/$NAME.jar
 cp deps/jama.jar $TMP/$NAME/library/
 cp deps/pca_transform.jar $TMP/$NAME/library/
+cp deps/jedis-2.9.0.jar $TMP/$NAME/library/
 
 echo "Copy the sources"
 # copy the source also
