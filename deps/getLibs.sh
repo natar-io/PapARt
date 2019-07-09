2d5115982beef27c7ce404543eb22ec7b9c13597
Jeremy Laviole <poqudrof@gmail.com>
2019-07-09 14:24:25 +0200

    stop destroying

--- deps/getLibs.sh
+++ deps/getLibs.sh
@@ -15,70 +15,70 @@ if [ ! -f toxiclibs-complete-0020.zip ]; then
     echo "Toxiclibs"
     wget https://bitbucket.org/postspectacular/toxiclibs/downloads/toxiclibs-complete-0020.zip
     unzip toxiclibs-complete-0020.zip
-    rm toxiclibs-complete-0020.zip
+#    rm toxiclibs-complete-0020.zip
 fi
 
 if [ ! -f SVGExtended.tgz ]; then
     echo "SVGExtended"
     wget https://github.com/Rea-lity-Tech/SVGExtended/releases/download/3.3.7.2/SVGExtended.tgz
     tar xvzf SVGExtended.tgz
-    rm SVGExtended.tgz
+#    rm SVGExtended.tgz
 fi
 
 if [ ! -f SimplePointCloud.tgz ]; then
     echo "SimplePointCloud"
     wget https://github.com/Rea-lity-Tech/SimplePointCloud/releases/download/1.0/SimplePointCloud.tgz
     tar xvzf SimplePointCloud.tgz
-    rm SimplePointCloud.tgz
+#    rm SimplePointCloud.tgz
 fi
 
 if [ ! -f ColorConverter.tgz ]; then
     echo "ColorConverter"
     wget https://github.com/Rea-lity-Tech/ColorConverter/releases/download/0.1/ColorConverter.tgz
     tar xvzf ColorConverter.tgz
-    rm ColorConverter.tgz
+#    rm ColorConverter.tgz
 fi
 
 if [ ! -f video-2.zip ]; then
     echo "Processing Video"
     wget https://github.com/processing/processing-video/releases/download/2/video-2.zip
     unzip video-2.zip
-    rm video-2.zip
+#    rm video-2.zip
 fi
 
 if [ ! -f processingTUIO.tgz ]; then
     echo "Processing TUIO"
     wget https://github.com/poqudrof/ProcessingTUIO/releases/download/0.5/processingTUIO.tgz
     tar xvzf processingTUIO.tgz
-    rm processingTUIO.tgz
+#    rm processingTUIO.tgz
 fi
 
 if [ ! -f oscP5-0.9.8.zip ]; then
     echo "OSCP5"
     wget http://www.sojamo.de/libraries/oscP5/download/oscP5-0.9.8.zip
     unzip oscP5-0.9.8.zip
-    rm oscP5-0.9.8.zip
+#    rm oscP5-0.9.8.zip
 fi
 
 if [ ! -f skatolo.tgz ]; then
     echo "Skatolo"
     wget https://github.com/Rea-lity-Tech/Skatolo/releases/download/1.1.1/skatolo.tgz
     tar xvzf skatolo.tgz
-    rm skatolo.tgz
+#    rm skatolo.tgz
 fi
 
 if [ ! -f guiModes.tgz ]; then
     echo "Gui Modes"
     wget https://github.com/poqudrof/guiModes/releases/download/0.5/guiModes.tgz
     tar xvzf guiModes.tgz
-    rm guiModes.tgz
+#    rm guiModes.tgz
 fi
 
 if [ ! -f peasycam_302.zip ]; then
     echo "Peasycam"
     wget http://mrfeinberg.com/peasycam/peasycam_302.zip
     unzip peasycam_302.zip
-    rm peasycam_302.zip
+#    rm peasycam_302.zip
 fi
 
 
